4d7d55cf1d06bfc66cd5269ed538bd4a1aa974f8
DJFancyAl <abluemleiv@gmail.com>
2023-07-01 11:04:52 -0400

    Created the functions_practice file

---
+++ functions_practice.py
@@ -0,0 +1,18 @@
+# hello Function
+def hello():
+    print('Welcome to my Function Practice Script!')
+
+
+# pack Function
+def pack(item1, item2, item3):
+    return [item1, item2, item3]
+
+
+# eat_lunch Function
+def eat_lunch(list):
+    if len(list) == 0:
+        print("My lunchbox is empty!")
+        return
+    
+    for item in list:
+        print()
