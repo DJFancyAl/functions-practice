770cc30c6466cfa9838d8e3fa212e7cc86bc8383
DJFancyAl <abluemleiv@gmail.com>
2023-07-01 11:14:39 -0400

    Completed the exercise

--- functions_practice.py
+++ functions_practice.py
@@ -14,8 +14,13 @@ def eat_lunch(list):
         print("My lunchbox is empty!")
         return
     
-    for item, index in list:
-        print(index)
+    for item in list:
+        if item == list[0]:
+            print(f"First I eat {item}.")
+        else:
+            print(f"Next I eat {item}.")
 
 
-eat_lunch()
+hello()
+print(pack('t-shirts', 'swimsuit', 'sunglasses'))
+eat_lunch(['Ham Sandwich', 'Fruit Cup', 'Chocolate Pudding'])
